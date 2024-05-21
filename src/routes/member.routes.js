76a54a6376f773c38e6c7e9e86c0a2edb158c55c
spykelionel <social.lionel@gmail.com>
2024-05-21 07:44:59 +0100

    add route def

--- src/routes/member.routes.js
+++ src/routes/member.routes.js
@@ -1,8 +1,17 @@
-const { create, getAllMembers } = require("../controllers/member.controller");
+const {
+  create,
+  getAllMembers,
+  updateMember,
+  deleteMember,
+} = require("../controllers/member.controller");
 const { verify } = require("../middlewares/auth.middleware");
 
 const memberRouter = require("express").Router();
 
-memberRouter.get("/", getAllMembers).post("/create", create);
+memberRouter
+  .get("/", getAllMembers)
+  .post("/create", create)
+  .put("/update/:id", updateMember)
+  .delete("/delete/:id", deleteMember);
 
 module.exports = memberRouter;
